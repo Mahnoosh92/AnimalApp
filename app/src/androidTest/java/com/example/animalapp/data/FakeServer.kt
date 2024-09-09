2e2b2f1211a63b43e60dbe051163d17f75107057
Mahnoosh92 <mahnooshmahdavi2012@gmail.com>
2024-09-09 09:33:39 +0100

    adding ui test

---
+++ app/src/androidTest/java/com/example/animalapp/data/FakeServer.kt
@@ -0,0 +1,5 @@
+package com.example.animalapp
+
+class FakeServer {
+    private val mockWebServer = MockWebServer()()
+}
---
+++ app/src/androidTest/java/com/example/animalapp/presentation/HomeScreenTest.kt
@@ -0,0 +1,4 @@
+package com.example.animalapp.presentation
+
+class HomeScreenTest {
+}
